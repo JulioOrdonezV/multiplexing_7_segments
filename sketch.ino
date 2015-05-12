ebed31671fc28f94c2defd43b88bdca234f7ff67
Julio Ordonez Viales <julio7k@gmail.com>
2015-05-11 19:30:08 -0600

    added comments, fixed style

--- sketch.ino
+++ sketch.ino
@@ -5,34 +5,38 @@ int decenas = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  DDRD=B11111111;
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
+  DDRD=B11111111; // pone los pines digitales del 0 - 7 en modo OUTPUT 
+  pinMode (8, OUTPUT);
+  pinMode (9, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  for(int i = 0; i <= 40; i++){
+
+  for (int i = 0; i <= 40; i++){ //enciende cada display 40 veces por segundo para eliminar parpadeo
+    
     unidades = contador%10;
-    decodificar(unidades);
-    digitalWrite(9, LOW);
-    digitalWrite(8, HIGH);
-    delay(12.5);
+    
+    decodificar (unidades);
+    digitalWrite (9, LOW);
+    digitalWrite (8, HIGH);
+    delay (12.5);
+    
     decenas = contador/10;
-    decodificar(decenas);
-    digitalWrite(9,HIGH);
-    digitalWrite(8,LOW);
-    delay(12.5);
+    
+    decodificar (decenas);
+    digitalWrite (9,HIGH);
+    digitalWrite (8,LOW);
+    delay (12.5);
   }
-  contador = (contador + 1)%60;
+  contador = (contador + 1)%60; //cuenta hasta 60 segundos
 }
 
-void decodificar(int val){
-  switch(val){
+void decodificar (int val){
+  switch (val){
     case 0:
-      PORTD = B01111110;
+      PORTD = B01111110; //escribe un byte directamente a los pines del PORTD (salidas digitales 0-7)
       break;
     case 1:
       PORTD = B00110000;
